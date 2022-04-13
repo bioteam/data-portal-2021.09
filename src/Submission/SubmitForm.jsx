10b49aba7accb4660ae5017e04ff6e76f932098a
vanetten <bill@bioteam.net>
2022-04-13 18:58:40 -0400

    sequence to cdph

--- src/Submission/SubmitForm.jsx
+++ src/Submission/SubmitForm.jsx
@@ -95,7 +95,7 @@ class SubmitForm extends Component {
     const { nodeTypes } = this.props.submission;
     const node = dictionary[this.state.chosenNode.value];
     // const options = nodeTypes.map((nodeType) => ({ value: nodeType, label: nodeType }));
-    const options = [{value: "case", label: "case"}, {value: "lab", label: "lab"}, {value: "sequence", label: "sequence"}];
+    const options = [{ value: 'case', label: 'case' }, { value: 'cdph', label: 'cdph' }, { value: 'lab', label: 'lab' }];
 
     const updateChosenNode = (newValue) => {
       this.setState({
